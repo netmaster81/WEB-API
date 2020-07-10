bce89864e8b63d7af30d4621b1fdc14b2649028b
ahmad <hafar.ahmad@gmail.com>
2020-07-10 14:08:04 +0200

    add fuuny

--- WEB-API/Controllers/WeatherForecastController.cs
+++ WEB-API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WEB_API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "funny", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
